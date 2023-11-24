b5b6085e7817b069190202ded001a64af9275b9b
Pedro de Oliveira <pedrooliveiraguimares@gmail.com>
2023-11-24 19:59:09 -0300

    Update Rodape.js

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -6,10 +6,10 @@ export default function Rodape(){
         <footer>
             <br/>
             <p>Laura Trujillo Rosal | RM: 550165 | 1TDSPJ | </p><br/>
-            <p>Maria Luiza de Oliveira | RM: 552169  | 1TDSPJ | </p><br/>
-            <p>Nicole Ribeiro Tuccio Rodrigues | RM: 99711   | 1TDSPJ | </p><br/>
-            <p>Pedro de Oliveira Guimarães | RM: 551367   | 1TDSPJ | </p><br/>
-            <p>Thiago Todeschini | RM: 551989   | 1TDSPJ | </p><br/>
+            <p>Maria Luiza de Oliveira | RM: 552169 | 1TDSPJ | </p><br/>
+            <p>Nicole Ribeiro Tuccio Rodrigues | RM: 99711 | 1TDSPJ | </p><br/>
+            <p>Pedro de Oliveira Guimarães | RM: 551367 | 1TDSPJ | </p><br/>
+            <p>Thiago Todeschini | RM: 551989 | 1TDSPJ | </p><br/>
         </footer>
     )
 }
